5ba70a741bcbdb78cc6f0315c7593c5714bdb839
Brandon Martel <brandonmartel@gmail.com>
2020-05-20 08:25:07 -0500

    fixing sw gen again??

--- src/sw/sw-template.js
+++ src/sw/sw-template.js
@@ -1,46 +1,37 @@
-if ("function" === typeof importScripts) {
-  importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-  );
-  if (workbox) {
-    /* injection point for manifest files.  */
-    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
 
-    workbox.routing.registerNavigationRoute(
-      workbox.precaching.getCacheKeyForURL("/index.html"),
-      {
-        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-      }
-    );
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg)$/,
-      new workbox.strategies.cacheFirst({
-        cacheName: "static-images",
-        plugins: [
-          new workbox.expiration.ExpirationPlugin({
-            maxEntries: 60,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          }),
-        ],
-      })
-    );
+workbox.routing.registerNavigationRoute("/index.html", {
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+});
 
-    // non precache
-    workbox.routing.registerRoute(
-      /\.(?:js|css)$/,
-      new workbox.strategies.CacheFirst({
-        cacheName: "static-resources",
-        plugins: [
-          new workbox.expiration.ExpirationPlugin({
-            maxEntries: 20,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          }),
-        ],
-      })
-    );
-  } else {
-    console.log("Workbox could not be loaded. No Offline support");
-  }
-}
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg)$/,
+  new workbox.strategies.cacheFirst({
+    cacheName: "static-images",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+// non precache
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "static-resources",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
 
