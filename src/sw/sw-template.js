c2c43f99b9c13d0dad8782ab92494e450c7b063d
Brandon Martel <brandonmartel@gmail.com>
2020-05-20 08:03:47 -0500

    fixng the sw gen

--- src/sw/sw-template.js
+++ src/sw/sw-template.js
@@ -1,27 +1,46 @@
-if ('function' === typeof importScripts) {
-    importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-    if (workbox) {
-        /* injection point for manifest files.  */
-        workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-        
-        workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('/index.html'), {
-            blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-        })
-        
-        workbox.routing.registerRoute(
-            /\.(?:png|gif|jpg|jpeg)$/,
-            new workbox.strategies.cacheFirst({
-                cacheName: 'images',
-                plugins: [
-                    new workbox.expiration.Plugin({
-                        maxEntries: 60,
-                        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                    }),
-                ],
-            })
-        );
+if ("function" === typeof importScripts) {
+  importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+  );
+  if (workbox) {
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute([]);
 
-    } else {
-        console.log('Workbox could not be loaded. No Offline support');
-    }
+    workbox.routing.registerNavigationRoute(
+      workbox.precaching.getCacheKeyForURL("/index.html"),
+      {
+        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+      }
+    );
+
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      new workbox.strategies.cacheFirst({
+        cacheName: "static-images",
+        plugins: [
+          new workbox.expiration.ExpirationPlugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      })
+    );
+
+    // non precache
+    workbox.routing.registerRoute(
+      /\.(?:js|css)$/,
+      new workbox.strategies.CacheFirst({
+        cacheName: "static-resources",
+        plugins: [
+          new workbox.expiration.ExpirationPlugin({
+            maxEntries: 20,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      })
+    );
+  } else {
+    console.log("Workbox could not be loaded. No Offline support");
+  }
 }
+
